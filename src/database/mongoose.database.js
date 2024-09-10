bf913fbb049b64746eea22a360ee34b52c24d025
Pedro Augusto Lopes Gomes <pedroaugustolopesgomes@gmail.com>
2024-09-09 22:21:41 -0300

    fix: atualizando conexão com o banco de dados

--- src/database/mongoose.database.js
+++ src/database/mongoose.database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectToDataBase = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@gerenciador.qfv4ei9.mongodb.net/?retryWrites=true&w=majority&appName=Gerenciador`
+      `mongodb+srv://${import.meta.env.VUE_APP_USER}:${import.meta.env.VUE_APP_PASSWORD}@apivue.2tf89.mongodb.net/?retryWrites=true&w=majority&appName=ApiVue`
     );
     console.log("Connected to MongoDB!");
   } catch (error) {
