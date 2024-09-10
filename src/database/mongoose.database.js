810dd1d72a2780be06d23f10d0ceabf4b9c7c334
Pedro Augusto Lopes Gomes <pedroaugustolopesgomes@gmail.com>
2024-09-09 22:27:58 -0300

    fix: atualizando para process.env

--- src/database/mongoose.database.js
+++ src/database/mongoose.database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectToDataBase = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${import.meta.env.VUE_APP_USER}:${import.meta.env.VUE_APP_PASSWORD}@apivue.2tf89.mongodb.net/?retryWrites=true&w=majority&appName=ApiVue`
+      `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@apivue.2tf89.mongodb.net/?retryWrites=true&w=majority&appName=ApiVue`
     );
     console.log("Connected to MongoDB!");
   } catch (error) {
